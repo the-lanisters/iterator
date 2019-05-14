b0494a20a27ca1e49ba750fff924fca160588b37
Jovan <jovankelly5@gmail.com>
2019-05-14 12:00:35 -0400

    renamed index.js to index.jsx, added App Component, created basic skeletons for login and signup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Auth/Login';
+import Signup from './Auth/Signup';
+
+const App = () => {
+  return (
+    // <div>My React App</div>
+    // <Login />
+    <Signup />
+  );
+};
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './App';
 
 if (module.hot) module.hot.accept();
-
-const App = () => {
-    return (
-        <div>My React App</div>
-    )
-};
 
 render(<App/>, document.querySelector('#root'));
