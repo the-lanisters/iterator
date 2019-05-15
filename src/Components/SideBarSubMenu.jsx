afb43e0259f78c7ed5699f59dc2564d574a20167
Jovan <jovankelly5@gmail.com>
2019-05-15 16:09:34 -0400

    slight sidebar changes

--- src/Components/SideBarSubMenu.jsx
+++ src/Components/SideBarSubMenu.jsx
@@ -1,31 +1,21 @@
 import React, { useState } from 'react';
 
 const styles = {
-  container: {
-    position: 'absolute',
-    width: '20em',
-    border: 'solid black 1px',
-    left: 0,
-    top: 0,
-    bottom: 0,
-    backgroundColor: 'grey',
-  },
   projectName: {
     marginBottom: '1em',
     backgroundColor: 'white',
   },
   membersContainer: {
     backgroundColor: 'green',
-    // height: '15em',
   },
   title: {
-    height: '2em',
+    height: '1em',
     padding: '0.5em',
     fontSize: '1.25em',
     borderBottom: 'solid 1px black',
   },
   member: {
-    padding: '1em',
+    padding: '0.5em',
     borderBottom: 'solid 1px black',
   },
   memberImg: {
@@ -33,16 +23,21 @@ const styles = {
     height: 'auto',
     verticalAlign: 'middle',
     margin: '0.25em',
+  },
+  inActive: {
+    display: 'none'
   }
 }
 
 const SideBarSubMenu = (props) => {
+  const [active, useActive] = useState(false);
+  let activeStyle = active ? {} : styles.inActive;
   return (
     <div style={styles.projectName}>
-        <div style={styles.title}>
+        <div style={styles.title} onClick={() => useActive(!active)}>
           Members
         </div>
-        <div style={styles.membersContainer}>
+        <div style={{...styles.membersContainer, ...activeStyle}}>
           <div style={styles.member}>
             <img style={styles.memberImg} src="http://chittagongit.com/download/96696"/> John Smith
           </div>
