10017a534e6a8a2994577d6470c63820e10ba9a2
Bo Peng <pb0928@gmail.com>
2019-05-16 11:11:04 -0400

    commiting small changes to pull

--- server/dbSetup.js
+++ server/dbSetup.js
@@ -2,7 +2,7 @@ const { connect, query } = require('./models/pmModel');
 const tables = require('./models/pmTables');
 
 (async function() {
-	console.log('Creating tables')
+	console.log('Creating tables');
 	await query({ text: tables.users });
 	await query({ text: tables.projects });
 	await query({ text: tables.userProjects })
